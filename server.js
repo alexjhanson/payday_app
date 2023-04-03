16144dd735aa8d8b71fc437324923d9e33c421d3
Alex Hanson <alexhanson@Alexs-MacBook-Air.local>
2023-04-02 17:20:24 -0700

    remove favico

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ const app = express();
 // mount middleware and configure static assets
 app.use(logger('dev'));
 app.use(express.json());
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 // routes
