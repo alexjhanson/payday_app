9d86e23747ab21692d36c5186fb75cf21e04beb3
Alex Hanson <alexhanson@Alexs-MacBook-Air.local>
2023-04-04 02:37:34 -0700

    .

--- controllers/employees.js
+++ controllers/employees.js
@@ -6,10 +6,7 @@ module.exports = {
 
 function index(req, res) {
     Employee.find({})
-    .then(result => {
-        console.log(result);
-        return res.status(200).json(result[0])
-    })
+    .then(result => res.status(200).json(result[0]))
     .catch(err => res.status(400).json(err))
 }
 
--- src/utils/initApp.js
+++ src/utils/initApp.js
@@ -1,7 +1,7 @@
 
 
 export default function initApp(setAppState) {
-     fetch('http://localhost:3001/employees')
+     fetch('/employees')
         .then(res => res.json())
         .then(employee => setAppState(employee));
 }
